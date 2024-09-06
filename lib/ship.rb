8752ae59b9c28b0f6e394137b04996b66712aae8
Danielle Cardona <danielle.cardona23@gmail.com>
2024-09-05 19:53:22 -0600

    instantiate Ship objects and create hit method

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,1 +1,18 @@
 class Ship
+
+    attr_reader :name,
+                :length,
+                :health, 
+
+    def initialize(name, length)
+        @name = name
+        @length = length
+        @health = length 
+        
+    end
+
+    def hit
+        @health -= 1
+    end
+
+end
