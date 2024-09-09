043005635b2f5c89ff759231140c1d78b75f4b9f
Danielle Cardona <danielle.cardona23@gmail.com>
2024-09-09 16:37:57 -0600

    add before each and initial ending the game test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,18 +1,33 @@
-require './lib/board'
-require './lib/ship'
-require './lib/cell'
+require 'spec_helper'
 require './lib/turn'
-require 'pry'
 
 RSpec.describe Turn do
     before(:each) do
         @turn = Turn.new
         @player_board = Board.new
         @cpu_board = Board.new
+        @cpu_board.create_cells
         @cpu_cruiser = Ship.new("Cruiser", 3)
         @cpu_submarine = Ship.new("Submarine", 2)
         @player_cruiser = Ship.new("Cruiser", 3)
         @player_submarine = Ship.new("Submarine", 2)
+
+        @cell_a1 = @cpu_board.cells["A1"]
+        @cell_a2 = @cpu_board.cells["A2"]
+        @cell_a3 = @cpu_board.cells["A3"]
+        @cell_b2 = @cpu_board.cells["B2"]
+        @cell_b3 = @cpu_board.cells["B3"]
+        @cell_c3 = @cpu_board.cells["C3"]
+        @cell_c4 = @cpu_board.cells["C4"]
+
+        @cell_a1.place_ship(@cpu_cruiser) 
+        @cell_a2.place_ship(@cpu_cruiser) 
+        @cell_a3.place_ship(@cpu_cruiser) 
+        @cpu_board.place(@cpu_cruiser, ["A1", "A2", "A3"])
+        @cell_c3.place_ship(@cpu_submarine) 
+        @cell_c4.place_ship(@cpu_submarine) 
+        @cpu_board.place(@cpu_submarine, ["C3", "C4"])
+        @cell_a1.fire_upon
     end
 
     describe '#initialze' do
@@ -23,16 +38,7 @@ RSpec.describe Turn do
 
     describe '#taking turns' do
         before(:each) do
-            @cpu_board.create_cells
-            @cell_a1 = @cpu_board.cells["A1"]
-            @cell_a2 = @cpu_board.cells["A2"]
-            @cell_a3 = @cpu_board.cells["A3"]
-            @cell_b2 = @cpu_board.cells["B2"]
-            @cell_a1.place_ship(@cpu_cruiser) 
-            @cell_a2.place_ship(@cpu_cruiser) 
-            @cell_a3.place_ship(@cpu_cruiser) 
-            @cpu_board.place(@cpu_cruiser, ["A1", "A2", "A3"])
-            @cell_a1.fire_upon
+            
         end
 
         it 'starts a players turn' do
@@ -52,12 +58,7 @@ RSpec.describe Turn do
         end
 
         it 'updates the board with the results of the shot' do
-            @cpu_board.create_cells
-            @cell_a1 = @cpu_board.cells["A1"]
-            @cell_b2 = @cpu_board.cells["B2"]
-            @cell_a1.place_ship(@cpu_cruiser) 
             @cpu_board.place(@cpu_cruiser, ["A1", "A2", "A3"])
-            @cell_a1.fire_upon
             @cell_b2.fire_upon
 
             @cpu_board.render
@@ -83,6 +84,17 @@ RSpec.describe Turn do
                         "C .  .  .  .  \n" +
                         "D .  .  .  .  \n"
             expect(@cpu_board.render).to eq(expected_render)
+        end
+    end
+
+    describe '#ending the game' do
+        it 'ends the game when all of a players ships are sunk' do
+            @cell_a2.fire_upon
+            @cell_a3.fire_upon
+            @cell_c3.fire_upon
+            @cell_c4.fire_upon
+
+            expect("Game over. Congratulations, you are the winner!!")
         end
     end
 end
