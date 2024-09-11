c52369e18e3c682bf24f95af58564591efd73fb4
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-10 21:18:27 -0500

    minor changes made to get game working properly

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,7 +1,6 @@
 class Cell
 
-    attr_reader :coordinate, :ship, :empty, :hit
-    attr_accessor :fire_upon
+    attr_reader :coordinate, :ship
 
     def initialize(coordinate)
         @coordinate = coordinate
